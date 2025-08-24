473c7908683125cbba2c4a776fe14a999861c759
kairen0606 <kairen060603@gmail.com>
2025-08-24 11:01:59 +0800

    Version 6

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -8,9 +8,9 @@ const Login: React.FC = () => {
   useEffect(() => {
     document.title = "Login Page";
   }, []);
-  
+
   const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
+    e.preventDefault(); 
 
     if (!email || !password) {
       alert("Please fill in all fields.");
@@ -52,11 +52,11 @@ const Login: React.FC = () => {
           <a href="/register" className="register-link">
             Register
           </a>
+
           <button type="submit" className="login-btn">
             Login
           </button>
         </div>
-
       </form>
     </div>
   );
